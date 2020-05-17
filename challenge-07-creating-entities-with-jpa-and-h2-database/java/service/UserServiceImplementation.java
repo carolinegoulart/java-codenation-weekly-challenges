8614d17e58a1128a1e4e05666ec1431cbb7d79e5
Caroline Goulart <45125835+carolinegoulart@users.noreply.github.com>
2020-05-17 02:01:29 -0300

    Add files via upload

---
+++ challenge-07-creating-entities-with-jpa-and-h2-database/java/service/UserServiceImplementation.java
@@ -0,0 +1,38 @@
+package com.challenge.service.implementation;
+
+import com.challenge.entity.User;
+import com.challenge.repository.UserRepository;
+import com.challenge.service.interfaces.UserServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImplementation implements UserServiceInterface {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Optional<User> findById(Long userId) {
+        return this.userRepository.findById(userId);
+    }
+
+    @Override
+    public User save(User user) {
+        return this.userRepository.save(user);
+    }
+
+    @Override
+    public List<User> findByAccelerationName(String name) {
+        return this.userRepository.findByAccelerationName(name);
+    }
+
+    @Override
+    public List<User> findByCompanyId(Long companyId) {
+        return this.userRepository.findByCompanyId(companyId);
+    }
+
+}

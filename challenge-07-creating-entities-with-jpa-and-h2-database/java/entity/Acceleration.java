aab57c543d0989bd956ead00c19040341030e1c0
Caroline Goulart <45125835+carolinegoulart@users.noreply.github.com>
2020-05-17 01:55:42 -0300

    Add files via upload

---
+++ challenge-07-creating-entities-with-jpa-and-h2-database/java/entity/Acceleration.java
@@ -0,0 +1,42 @@
+package com.challenge.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="acceleration")
+@EntityListeners(AuditingEntityListener.class)
+public class Acceleration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Size(max=100)
+    private String name;
+
+    @NotNull
+    @Size(max=50)
+    private String slug;
+
+    @ManyToOne
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date created_at;
+
+    @OneToMany(mappedBy = "acceleration")
+    private List<Candidate> candidates;
+
+    public Acceleration() { }
+
+}
